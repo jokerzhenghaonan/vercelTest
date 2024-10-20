48f1e73bd5b93558a1ba5b0304690c99cd666ec4
jokerzhenghaonan <1289147676@qq.com>
2024-10-20 17:45:27 +0800

    vercelTest1

--- app/api/download/audio.ts
+++ app/api/download/audio.ts
@@ -226,119 +226,68 @@ import chromium from "@sparticuz/chromium-min";
 // import axios from 'axios';  
 // import fs from 'fs';  
 // const { getRandomIPv6 } = require("@distube/ytdl-core/lib/utils");
-// const cookies = [
-//     {
-//         "domain": ".youtube.com",
-//         "expirationDate": 1759040170.880122,
-//         "hostOnly": false,
-//         "httpOnly": false,
-//         "name": "__Secure-1PSIDTS",
-//         "path": "/",
-//         "sameSite": "no_restriction",
-//         "secure": false,
-//         "session": false,
-//         "storeId": "0",
-//         "value": "sidts-CjEBQlrA-IW_mAlMjLElKrChS71Si-VLm-hjC96vGin20e6p0n9ZLgfWZr6w4Oh11JIhEAA",
-//         "id": 1
-//     },
-//     {
-//         "domain": ".youtube.com",
-//         "expirationDate": 1761042620,
-//         "hostOnly": false,
-//         "httpOnly": false,
-//         "name": "__Secure-3PAPISID",
-//         "path": "/",
-//         "sameSite": "no_restriction",
-//         "secure": false,
-//         "session": false,
-//         "storeId": "0",
-//         "value": "nxX4aSyTGEIxx8HK/An5-xX7VL_HcQxvNm",
-//         "id": 2
-//     },
-//     {
-//         "domain": ".youtube.com",
-//         "expirationDate": 1761042620,
-//         "hostOnly": false,
-//         "httpOnly": false,
-//         "name": "__Secure-3PSID",
-//         "path": "/",
-//         "sameSite": "no_restriction",
-//         "secure": false,
-//         "session": false,
-//         "storeId": "0",
-//         "value": "g.a000nwhWwpYJbjKww4aR14Ghe8Qi-0njqkSjnbgXl9yDfLBZKyWY8Gwq6b-fEuOR86vOIdFSkQACgYKAeQSARASFQHGX2Mi9NW5eEwGI5j_aRdW5SSHwhoVAUF8yKq9_lNbbr4aMGj1eNJoNyLG0076",
-//         "id": 3
-//     },
-//     {
-//         "domain": ".youtube.com",
-//         "expirationDate": 1759040531.533088,
-//         "hostOnly": false,
-//         "httpOnly": false,
-//         "name": "__Secure-3PSIDCC",
-//         "path": "/",
-//         "sameSite": "no_restriction",
-//         "secure": false,
-//         "session": false,
-//         "storeId": "0",
-//         "value": "AKEyXzXs4VSXEuc29cPbyXzkq9gO-Tego38szJ1tIVWXfrCPPlZ4N4jFA73nGdyLQZ-akWMYffw",
-//         "id": 4
-//     },
-//     {
-//         "domain": ".youtube.com",
-//         "expirationDate": 1759040170.880185,
-//         "hostOnly": false,
-//         "httpOnly": false,
-//         "name": "__Secure-3PSIDTS",
-//         "path": "/",
-//         "sameSite": "no_restriction",
-//         "secure": false,
-//         "session": false,
-//         "storeId": "0",
-//         "value": "sidts-CjEBQlrA-IW_mAlMjLElKrChS71Si-VLm-hjC96vGin20e6p0n9ZLgfWZr6w4Oh11JIhEAA",
-//         "id": 5
-//     },
-//     {
-//         "domain": ".youtube.com",
-//         "expirationDate": 1761042620,
-//         "hostOnly": false,
-//         "httpOnly": false,
-//         "name": "LOGIN_INFO",
-//         "path": "/",
-//         "sameSite": "no_restriction",
-//         "secure": false,
-//         "session": false,
-//         "storeId": "0",
-//         "value": "AFmmF2swRAIgYlft9b09IyYe5podBDomFxF4F-jVa4Y0OaEHnNJL3wACIDy9I42UEzcmN0OUlA3ILXSAz5E7ZCg91Sxz1JJg-XIR:QUQ3MjNmd1V6ZDN5MlpuMlF6d0FCckZlSHFjZU1TTDR5eThxemFuaTRKN25HNEpweTlUTGd1WGs4cVhpZEtfMFBYeW5NYi1ybkhYOUk4Mkx4cVJJRmJFNmx2M2JRWTh3VXVxaFN2a3pFcnI0aWNhZGdONEQyRTJKVEJzeW03b2U2Z1RhbHljMEtycDM3cVhhSnQzaFkwVk81ZUxjaXZ3a3dR",
-//         "id": 6
-//     },
-//     {
-//         "domain": ".youtube.com",
-//         "expirationDate": 1762055340,
-//         "hostOnly": false,
-//         "httpOnly": false,
-//         "name": "PREF",
-//         "path": "/",
-//         "sameSite": "no_restriction",
-//         "secure": false,
-//         "session": false,
-//         "storeId": "0",
-//         "value": "f7=4100&tz=Asia.Shanghai&f4=10000",
-//         "id": 7
-//     }
-//     ]
+const cookies =[
+ //{
+//   name: 'VISITOR_INFO1_LIVE',
+//   value: 'UEtBReTMbTs',
+//   domain: '.youtube.com',
+//   path: '/',
+//   expires: 1744967688.844423,
+//   size: 29,
+//   httpOnly: true,
+//   secure: true,
+//   session: false,
+//   sameSite: 'None',
+//   priority: 'Medium',
+//   sameParty: false,
+//   sourceScheme: 'Secure',
+//   partitionKey: undefined
+// },
+// {
+//   name: 'YSC',
+//   value: 'eFQdNqZjMLw',
+//   domain: '.youtube.com',
+//   path: '/',
+//   expires: -1,
+//   size: 14,
+//   httpOnly: true,
+//   secure: true,
+//   session: true,
+//   sameSite: 'None',
+//   priority: 'Medium',
+//   sameParty: false,
+//   sourceScheme: 'Secure',
+//   partitionKey: undefined
+// },
+// {
+//   name: 'GPS',
+//   value: '1',
+//   domain: '.youtube.com',
+//   path: '/',
+//   expires: 1729417488.844231,
+//   size: 4,
+//   httpOnly: true,
+//   secure: true,
+//   session: false,
+//   priority: 'Medium',
+//   sameParty: false,
+//   sourceScheme: 'Secure',
+//   partitionKey: undefined
+// }
+]
 
 
-// const agent = ytdl.createProxyAgent({uri: "http://127.0.0.1:33210"}, cookies
+// const agent = ytdl.createProxyAgent({uri: "http://127.0.0.1:7890"}, cookies
 // );
 // const agentForARandomIP = ytdl.createAgent(undefined, {
 //     localAddress: getRandomIPv6("2001:2::/48"),
 //   });
 async function tryProcessAudio(url: string, retries = 0){
     console.log(`开始处理 YouTube 音频, URL: ${url}, 剩余重试次数: ${retries}`);
-    const cookieGet = await GET();
-    // const agent = ytdl.createProxyAgent({uri: "http://127.0.0.1:33210"}, cookieGet);
-    const agent = ytdl.createAgent(cookieGet);
-    console.log(agent, "55555")
+  //  const cookieGet = await GET();
+     const agent = ytdl.createProxyAgent({uri: "http://127.0.0.1:7890"}, cookies);
+  // const agent = ytdl.createAgent(cookies);
+  //  console.log(agent, "55555")
     const videoInfo = await ytdl.getInfo(url, { agent: agent });  
     console.log('获取到视频信息', videoInfo);  
     
