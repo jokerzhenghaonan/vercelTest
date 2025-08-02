93eb0a9423ee6a502f3e613c28f3f57eb702ef28
jokerzhenghaonan <1289147676@qq.com>
2025-08-03 00:49:29 +0800

    给倩欣测试github

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex flex-col justify-center items-center min-h-screen p-4">
+      <main className="flex flex-col justify-center items-center min-h-screen ">
         <Search />
       </main>
     </>
